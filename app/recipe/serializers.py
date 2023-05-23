57f31e0deed3f28769a3d60c79fdee63ab3cb08b
markkhramovich <mark.khramovich@digitalinspirationlabs.io>
2023-05-23 12:48:01 +0300

    Add tags

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
@@ -11,6 +13,12 @@ class RecipeSerializer(serializers.ModelSerializer):
 
 
 class RecipeDetailSerializer(RecipeSerializer):
-
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
